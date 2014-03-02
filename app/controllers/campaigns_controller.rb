bc137a8376bbffbfc206127e3ad79b1c3b40381f
Yaron <ysadka@gmail.com>
2014-03-02 12:55:18 -0800

    campaign MVC

---
+++ app/controllers/campaigns_controller.rb
@@ -0,0 +1,43 @@
+class CampaignsController < ApplicationController
+
+  def index
+    @campaigns = Campaign.all
+  end
+
+  def new
+    @campaign = Campaign.new
+  end
+
+  def create
+    campaign = Campaign.new(campaign_params)
+    if campaign.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @campaign = Campaign.find_by(id: params[:id])
+  end
+
+  def update
+    
+  end
+
+  def edit
+    
+  end
+
+  def destroy
+    campaign = Campaign.find_by(id: params[:id])
+    campaign.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def campaign_params
+    params.require(:campaign).permit(:name, :end_date, :funds_raised, :description)
+  end
+end
