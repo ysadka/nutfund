f0e8527850442ae89a915ccd45cde0ea19756cd9
Yaron <ysadka@gmail.com>
2014-03-02 17:19:01 -0800

    make a new payment...used nested resources bitches! Woooo!!

---
+++ app/controllers/payments_controller.rb
@@ -0,0 +1,35 @@
+class PaymentsController < ApplicationController
+
+  def new
+    @campaign = Campaign.find_by(id: params[:campaign_id])
+    @payment = Payment.new(campaign: @campaign)
+  end
+
+  def create
+    payment = Payment.new(payment_params)
+    if payment.save
+      redirect_to campaign_path(params[:campaign_id])
+    else
+      render :new
+    end
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def payment_params
+    params.require(:payment).permit(:campaign_id,
+                                    :user_id,
+                                    :date, 
+                                    :amount, 
+                                    :comment
+                                   )
+  end
+end
