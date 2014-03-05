473f7eefad008c346aa1cf29650df6a181aa40c0
Yaron <ysadka@gmail.com>
2014-03-05 09:59:48 -0600

    round btc to 3 points

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   def launch
     coinbase = Coinbase::Client.new(ENV['COINBASE_API_KEY'], ENV['COINBASE_API_SECRET'])
-    @amount_raised_btc = coinbase.balance.to_f
+    @amount_raised_btc = coinbase.balance.to_f.round(3)
     @amount_raised_usd = (@amount_raised_btc * get_updated_rate).round(2)
     render layout: 'launch'
   end
