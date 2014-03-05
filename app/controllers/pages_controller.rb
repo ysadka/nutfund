ccab19f53f7f2b445ca8fbffc525b57e28ebed51
Blake Benthall <blake@benthall.net>
2014-03-05 07:56:36 -0600

    Tweaks, updated copy

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   def launch
-    @amount_raised_usd = 3020
-    @amount_raised_btc = 4.083
+    @amount_raised_btc = 4.6894
+    @amount_raised_usd = (@amount_raised_btc * 644.6).round
     render layout: 'launch'
   end
 
